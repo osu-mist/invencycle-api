55efe9c9a53a82760018eb6d9781d3ae36dc3cd1
jaredkosanovic <jared.tennismaster@gmail.com>
2016-07-14 12:59:59 -0700

    Added POST to DAO and bikeresource

--- src/main/groovy/edu/oregonstate/mist/invencycle/db/BikeDAO.groovy
+++ src/main/groovy/edu/oregonstate/mist/invencycle/db/BikeDAO.groovy
@@ -11,6 +11,118 @@ import org.skife.jdbi.v2.sqlobject.Bind
 public interface BikeDAO extends Closeable {
 
     /**
+     * POST a bike
+     */
+    @SqlUpdate("""
+              INSERT INTO BRAKE (BRAKE_ID, BRAKE_MAKE, BRAKE_MODEL, BRAKE_ROTOR_SIZE, BRAKE_POSITION)
+              VALUES (BIKE_BRAKE_ID_SEQ.nextval, :brake_make_front, :brake_model_front, :brake_rotor_size_front, 'Front')
+               """)
+    void postBrakeFront(@Bind("brake_make_front") String brake_make_front,
+                        @Bind("brake_model_front") String brake_model_front,
+                        @Bind("brake_rotor_size_front") Integer brake_rotor_size_front)
+
+    @SqlUpdate("""
+              INSERT INTO BRAKE (BRAKE_ID, BRAKE_MAKE, BRAKE_MODEL, BRAKE_ROTOR_SIZE, BRAKE_POSITION)
+              VALUES (BIKE_BRAKE_ID_SEQ.nextval, :brake_make_rear, :brake_model_rear, :brake_rotor_size_rear, 'Rear')
+               """)
+    void postBrakeRear(@Bind("brake_make_rear") String brake_make_rear,
+                        @Bind("brake_model_rear") String brake_model_rear,
+                        @Bind("brake_rotor_size_rear") Integer brake_rotor_size_rear)
+
+    @SqlUpdate("""
+              INSERT INTO DERAILUER (DERAILUER_ID, DERAILUER_MAKE, DERAILUER_MODEL, DERAILUER_SPEEDS, DERAILUER_POSITION)
+              VALUES (BIKE_DERAILUER_ID_SEQ.nextval, :derailuer_make_front, :derailuer_model_front, :derailuer_speeds_front, 'Front')
+              """)
+    void postDerailuerFront(@Bind("derailuer_make_front") String derailuer_make_front,
+                            @Bind("derailuer_model_front") String derailuer_model_front,
+                            @Bind("derailuer_speeds_front") Integer derailuer_speeds_front)
+
+    @SqlUpdate("""
+              INSERT INTO DERAILUER (DERAILUER_ID, DERAILUER_MAKE, DERAILUER_MODEL, DERAILUER_SPEEDS, DERAILUER_POSITION)
+              VALUES (BIKE_DERAILUER_ID_SEQ.nextval, :derailuer_make_rear, :derailuer_model_rear, :derailuer_speeds_rear, 'Rear')
+              """)
+    void postDerailuerRear(@Bind("derailuer_make_rear") String derailuer_make_rear,
+                            @Bind("derailuer_model_rear") String derailuer_model_rear,
+                            @Bind("derailuer_speeds_rear") Integer derailuer_speeds_rear)
+    
+    @SqlUpdate("""
+              INSERT INTO FRAME_SIZE (FRAME_SIZE_ID, FRAME_SIZE_NAME, FRAME_SIZE_CM)
+              VALUES (BIKE_FRAME_SIZE_SEQ.nextval, :frame_size_name, :frame_size_cm)
+               """)
+    void postFrameSize(@Bind("frame_size_name") String frame_size_name,
+                       @Bind("frame_size_cm") Integer frame_size_cm)
+    
+    @SqlUpdate("""
+              INSERT INTO SUSPENSION (SUSPENSION_ID, SUSPENSION_MAKE, SUSPENSION_MODEL, SUSPENSION_TRAVEL, SUSPENSION_TYPE)
+              VALUES (BIKE_SUSPENSION_ID_SEQ.nextval, :fork_make, :fork_model, :fork_travel, 'Fork')
+               """)
+    void postFork(@Bind("fork_make") String fork_make,
+                  @Bind("fork_model") String fork_model,
+                  @Bind("fork_travel") Integer fork_travel)
+
+    @SqlUpdate("""
+              INSERT INTO SUSPENSION (SUSPENSION_ID, SUSPENSION_MAKE, SUSPENSION_MODEL, SUSPENSION_TRAVEL, SUSPENSION_TYPE)
+              VALUES (BIKE_SUSPENSION_ID_SEQ.nextval, :shock_make, :shock_model, :shock_travel, 'Shock')
+               """)
+    void postShock(@Bind("shock_make") String shock_make,
+                  @Bind("shock_model") String shock_model,
+                  @Bind("shock_travel") Integer shock_travel)
+    
+    @SqlUpdate("""
+               INSERT INTO WHEEL (WHEEL_ID, WHEEL_SIZE, WHEEL_RIM_MAKE, WHEEL_RIM_MODEL, WHEEL_HUB_MAKE, WHEEL_HUB_MODEL, WHEEL_POSITION)
+               VALUES (BIKE_WHEEL_ID_SEQ.nextval, :wheel_size_front, :rim_make_front, :rim_model_front, :hub_make_front, :hub_model_front, 'Front')
+              """)
+    void postWheelFront(@Bind("wheel_size_front") Integer wheel_size_front,
+                        @Bind("rim_make_front") String rim_make_front,
+                        @Bind("rim_model_front") String rim_model_front,
+                        @Bind("hub_make_front") String hub_make_front,
+                        @Bind("hub_model_front") String hub_model_front)
+
+    @SqlUpdate("""
+               INSERT INTO WHEEL (WHEEL_ID, WHEEL_SIZE, WHEEL_RIM_MAKE, WHEEL_RIM_MODEL, WHEEL_HUB_MAKE, WHEEL_HUB_MODEL, WHEEL_POSITION)
+               VALUES (BIKE_WHEEL_ID_SEQ.nextval, :wheel_size_rear, :rim_make_rear, :rim_model_rear, :hub_make_rear, :hub_model_rear, 'Rear')
+              """)
+    void postWheelRear(@Bind("wheel_size_rear") Integer wheel_size_rear,
+                        @Bind("rim_make_rear") String rim_make_rear,
+                        @Bind("rim_model_rear") String rim_model_rear,
+                        @Bind("hub_make_rear") String hub_make_rear,
+                        @Bind("hub_model_rear") String hub_model_rear)
+
+    @SqlUpdate("""
+              INSERT INTO BIKE (BIKE_ID, BIKE_MAKE, BIKE_MODEL, BIKE_YEAR, BIKE_BIKE_TYPE_ID, BIKE_FRAME_SIZE_ID, BIKE_MSRP, BIKE_DERAILUER_FRONT_ID, BIKE_DERAILUER_REAR_ID, BIKE_FORK_ID, BIKE_SHOCK_ID, BIKE_WHEEL_FRONT_ID, BIKE_WHEEL_REAR_ID, BIKE_BRAKE_FRONT_ID, BIKE_BRAKE_REAR_ID, BIKE_CREATE_DATE)
+              VALUES (BIKE_ID_SEQ.nextval, :bike_make, :bike_model, :bike_year,
+
+                (SELECT BIKE_TYPE_ID FROM BIKE_TYPE
+                WHERE BIKE_TYPE_NAME LIKE :bike_type),
+
+                (SELECT MAX(FRAME_SIZE_ID) FROM FRAME_SIZE),
+
+              :bike_msrp,
+
+                (SELECT MAX(DERAILUER_ID) FROM DERAILUER WHERE DERAILUER_POSITION LIKE 'Front'),
+
+                (SELECT MAX(DERAILUER_ID) FROM DERAILUER WHERE DERAILUER_POSITION LIKE 'Rear'),
+
+                (SELECT MAX(SUSPENSION_ID) FROM SUSPENSION WHERE SUSPENSION_TYPE LIKE 'Fork'),
+
+                (SELECT MAX(SUSPENSION_ID) FROM SUSPENSION WHERE SUSPENSION_TYPE LIKE 'Shock'),
+
+                (SELECT MAX(WHEEL_ID) FROM WHEEL WHERE WHEEL_POSITION LIKE 'Front'),
+
+                (SELECT MAX(WHEEL_ID) FROM WHEEL WHERE WHEEL_POSITION LIKE 'Rear'),
+
+                (SELECT MAX(BRAKE_ID) FROM BRAKE WHERE BRAKE_POSITION LIKE 'Front'),
+
+                (SELECT MAX(BRAKE_ID) FROM BRAKE WHERE BRAKE_POSITION LIKE 'Rear'),
+
+              SYSDATE)
+              """)
+    void postBike(@Bind("bike_make") String make,
+                  @Bind("bike_model") String model,
+                  @Bind("bike_msrp") Integer msrp,
+                  @Bind("bike_year") Integer year,
+                  @Bind("bike_type") String type)
+    /**
      * Delete by ID
      */
     @SqlUpdate("""
--- src/main/groovy/edu/oregonstate/mist/invencycle/resources/BikeResource.groovy
+++ src/main/groovy/edu/oregonstate/mist/invencycle/resources/BikeResource.groovy
@@ -8,12 +8,15 @@ import com.google.common.base.Optional
 
 import javax.ws.rs.DELETE
 import javax.ws.rs.GET
+import javax.ws.rs.POST
 import javax.ws.rs.Path
 import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
 import javax.ws.rs.core.Response
 import javax.ws.rs.core.MediaType
 import javax.ws.rs.QueryParam
+import javax.validation.Valid
+import javax.ws.rs.Consumes
 
 /**
  *Bike resource class
@@ -30,7 +33,7 @@ class BikeResource extends Resource {
     }
 
     /**
-     *Get by ID
+     *GET by ID
      */
     @GET
     @Path ('{id: \\d+}')
@@ -54,7 +57,7 @@ class BikeResource extends Resource {
     }
 
     /**
-     * Get by query
+     * GET by query
      */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -66,7 +69,56 @@ class BikeResource extends Resource {
     }
 
     /**
-     * Delete by ID
+     * POST bikes
+     */
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response postBike (@Valid Bike newBike) {
+        Response returnResponse
+        //try {
+            bikeDAO.postBrakeFront(newBike.brake_make_front,
+                                   newBike.brake_model_front,
+                                   newBike.brake_rotor_size_front)
+            bikeDAO.postBrakeRear(newBike.brake_make_rear,
+                                  newBike.brake_model_rear,
+                                  newBike.brake_rotor_size_rear)
+            bikeDAO.postDerailuerFront(newBike.derailuer_make_front,
+                                       newBike.derailuer_model_front,
+                                       newBike.derailuer_speeds_front)
+            bikeDAO.postDerailuerRear(newBike.derailuer_make_rear,
+                                      newBike.derailuer_model_rear,
+                                      newBike.derailuer_speeds_rear)
+            bikeDAO.postFrameSize(newBike.frame_size_cm,
+                                  newBike.frame_size_name)
+            bikeDAO.postFork(newBike.fork_make,
+                             newBike.fork_model,
+                             newBike.fork_travel)
+            bikeDAO.postShock(newBike.shock_make,
+                              newBike.shock_model,
+                              newBike.shock_travel)
+            bikeDAO.postWheelFront(newBike.wheel_size_front,
+                                   newBike.rim_make_front,
+                                   newBike.rim_model_front,
+                                   newBike.hub_make_front,
+                                   newBike.hub_model_front)
+            bikeDAO.postWheelRear(newBike.wheel_size_rear,
+                                  newBike.rim_make_rear,
+                                  newBike.rim_model_rear,
+                                  newBike.hub_make_rear,
+                                  newBike.hub_model_rear)
+            bikeDAO.postBike(newBike.make,
+                             newBike.model,
+                             newBike.msrp,
+                             newBike.year,
+                             newBike.type)
+        //} catch (Exception e) {
+         //   returnResponse = internalServerError("Internal server error").build()
+       // }
+    }
+
+    /**
+     * DELETE by ID
      */
     @DELETE
     @Path('{id: \\d+}')
